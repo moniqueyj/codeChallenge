d5a0a8bbaa076ed52e0520f65721b9f330fc97c6
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-09-14 13:44:59 -0700

    it populates data on the table

--- app/component/create-data-table/index.js
+++ app/component/create-data-table/index.js
@@ -6,15 +6,31 @@ const sampleApp = angular.module('sampleApp');
 sampleApp.component('createDataTable',{
   template: require('./create-data-table.html'),
   controller: 'CreateTableController',
-  controllerAs:'creatteTableCtrl'
+  controllerAs:'createTableCtrl'
 });
 
-sampleApp.controller('CreateTableController', ['$scope', '$http', CreateTableController]);
-function CreateTableController($scope, $http){
-  var list = { content:null};
-  $http.get('../../data/basedata.json').success(function(data){
-    list.content = data;
+sampleApp.factory('listsFactory', function($q,$http){
+  return {
+    getLists: function(){
+      var deferred = $q.defer(),
+        httpPromise = $http.get('https://gist.githubusercontent.com/evanjacobs/c150c0375030dc4de65e9b95784dc894/raw/35c5f455b147703db3989df0cb90f5781c3b312f/usage_data.json');
+      httpPromise.then(function(response){
+        deferred.resolve(response);
+      }, function(error){
+        console.error(error);
+      });
+      return deferred.promise;
+    }
+  };
+});
+
+sampleApp.controller('CreateTableController', ['$scope', '$http','listsFactory', CreateTableController]);
+
+function CreateTableController($scope,$http, listsFactory){
+  listsFactory.getLists().then(function(response){
+    $scope.lists = response.data;
+    console.log($scope.lists);
+  }, function(error){
+    console.error(error);
   });
-  console.log(list);
-  return list;
 }
